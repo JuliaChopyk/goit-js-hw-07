c248b7d1172ba1bbaadb48ac454fb28a5d4f2e2a
JuliaChopyk <chopyk.julia@gmail.com>
2023-07-16 22:03:19 +0300

    homework 07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  createImageCardsMarkup(galleryItems)
+);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, preview, original }) => {
+      return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+  <div class="modal">
+<img
+      class="gallery__image"
+      src="${evt.target.dataset.source}" width = "800"
+      alt="${evt.target.alt}"
+    />
+    </div>
+`);
+
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  createImageCardsMarkup(galleryItems)
+);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, preview, original }) => {
+      return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
